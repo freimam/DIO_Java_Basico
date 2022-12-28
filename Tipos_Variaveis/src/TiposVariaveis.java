6fdf2138337f9d707630810f30e1ee125bcfe81d
Freimam <freimam@gmail.com>
2022-12-28 16:37:55 -0300

    Tipos Variaveis JAVA

---
+++ Tipos_Variaveis/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        int idade;
+        //anoFrabricacao valor atribuido pode mudar
+        int anoFabricacao = 2020;
+        anoFabricacao = 2022;
+        //VALOR_DE_PI valor não pode mudar
+        final double VALOR_DE_PI = 3.14;
+
+    }
+}
